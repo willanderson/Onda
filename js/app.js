6dd24502300eab9e33bc1bba1f675608a51dc3f7
Will Anderson <wmaxand@gmail.com>
2020-04-14 23:23:42 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+  var image = document.getElementsByClassName('lax');
+  new simpleParallax(image, {
+    orientation: 'up',
+    overflow: true
+  });
+
+
+
+
+
+
+});
