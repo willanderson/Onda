47c6b99b57eef6b05423f0ab32be064cf858775d
Will Anderson <wmaxand@gmail.com>
2020-04-20 21:48:57 -0400

    Added age gate

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+  var yes21 = document.getElementById('21Yes');
+  var no21 = document.getElementById('21No');
+
+  $(yes21).click(function() {
+    $('body').removeClass('modal-open');
+    window.scrollTo(0,0);
+  })
+
+  $(no21).click(function() {
+    window.location.href = 'http://snapple.com';
+  });
+
 
   var image = document.getElementsByClassName('lax');
   new simpleParallax(image, {
@@ -19,6 +31,8 @@ $(document).ready(function() {
       }
     });
   });
+
+
 
 
 
