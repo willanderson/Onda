3fcab32104f1a6c0e703cc02eafd48e0f5c03b6d
Will Anderson <wmaxand@gmail.com>
2020-04-20 21:06:09 -0400

    Latest style update

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ $(document).ready(function() {
     overflow: true
   });
 
+  var stickyOffset = $("#navbar").offset();
+  var $contentDivs = $(".content");
+  $(document).scroll(function() {
+    $contentDivs.each(function(k) {
+      var _thisOffset = $(this).offset();
+      var _actPosition = _thisOffset.top - $(window).scrollTop();
+      if (_actPosition < stickyOffset.top && _actPosition + $(this).height() > 0) {
+        $("#navbar").removeClass("green-nav red-nav").addClass($(this).hasClass("green-nav") ? "green-nav" : "red-nav");
+        return false;
+      }
+    });
+  });
+
+
 
 
 
